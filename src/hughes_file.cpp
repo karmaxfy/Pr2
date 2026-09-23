2d4e0389cf5dab263cc563fd0aad8102ec02e959
karmaxfy <100592820+karmaxfy@users.noreply.github.com>
2026-09-23 10:56:19 +0700

    hughes_file.cpp

--- src/hughes_file.cpp
+++ src/hughes_file.cpp
@@ -11,15 +11,11 @@ void run_hughes_file(std::ostream& log) {
     const std::string encrypted_path = "data/encrypted_hughes.txt";
     const std::string decrypted_path = "data/decrypted.txt";
 
-    // Общие открытые параметры протокола Хьюза.
     const int p = 331;
     const int g = 3;
 
-    // Секрет Алисы.
     const int x = 37;
 
-    // Секрет Боба. Важно: НОД(y, p - 1) должен быть равен 1,
-    // чтобы существовал обратный показатель z = y^(-1) mod (p - 1).
     const int y = 17;
 
     std::ostringstream silent;
@@ -30,14 +26,11 @@ void run_hughes_file(std::ostream& log) {
     log << "Открытые параметры: p = " << p << ", g = " << g << ".\n";
     log << "Секрет Алисы x = " << x << ", секрет Боба y = " << y << ".\n\n";
 
-    // 1. Алиса генерирует секретный ключ K = g^x mod p.
     int key_alice = mod_pow_binary(g, x, p, silent);
     log << "1) Алиса генерирует ключ:\n";
     log << "K = g^x mod p = " << g << "^" << x << " mod " << p
         << " = " << key_alice << ".\n\n";
 
-    // 2. Боб выбирает y, взаимно простое с p - 1,
-    // и отправляет Алисе Y = g^y mod p.
     int gcd_y = gcd_int(y, p - 1);
     log << "2) Проверяем показатель Боба:\n";
     log << "НОД(" << y << ", " << p - 1 << ") = " << gcd_y << ".\n";
@@ -52,14 +45,11 @@ void run_hughes_file(std::ostream& log) {
     log << "Y = g^y mod p = " << g << "^" << y << " mod " << p
         << " = " << Y << ".\n\n";
 
-    // 3. Алиса возводит полученное Y в свою секретную степень x
-    // и отправляет Бобу X = Y^x mod p.
     int X = mod_pow_binary(Y, x, p, silent);
     log << "3) Алиса вычисляет и отправляет Бобу:\n";
     log << "X = Y^x mod p = " << Y << "^" << x << " mod " << p
         << " = " << X << ".\n\n";
 
-    // 4. Боб находит обратный показатель z к y по модулю p - 1.
     int z = mod_inverse(y, p - 1, silent);
     log << "4) Боб находит обратный показатель:\n";
     log << "z = y^(-1) mod (p - 1) = " << y << "^(-1) mod " << p - 1
@@ -67,7 +57,6 @@ void run_hughes_file(std::ostream& log) {
     log << "Проверка: " << y << " * " << z << " mod " << p - 1
         << " = " << (y * z) % (p - 1) << ".\n\n";
 
-    // 5. Боб восстанавливает ключ Алисы.
     int key_bob = mod_pow_binary(X, z, p, silent);
     log << "5) Боб восстанавливает общий ключ:\n";
     log << "K' = X^z mod p = " << X << "^" << z << " mod " << p
@@ -82,10 +71,6 @@ void run_hughes_file(std::ostream& log) {
 
     log << "Ключи совпали. Протокол Хьюза выполнен успешно.\n\n";
 
-    // После протокола обе стороны знают один и тот же ключ.
-    // Для учебного шифрования файла используем умножение по модулю p:
-    // c = m * K mod p.
-    // Расшифрование: m = c * K^(-1) mod p.
     int key_inverse = mod_inverse(key_bob, p, silent);
 
     std::vector<unsigned char> input = read_binary_file(input_path);
